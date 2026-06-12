8cd155bcb651e62952ad06b21d7d5321d32f932f
sean <dsean120577@gmail.com>
2026-06-12 15:56:39 +0800

    Initial Commit

---
+++ InterviewPractice/InterviewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewPracticeApp.swift
+//  InterviewPractice
+//
+//  Created by potentia on 2026/6/12.
+//
+
+import SwiftUI
+
+@main
+struct InterviewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InterviewPracticeTests/InterviewPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewPracticeTests.swift
+//  InterviewPracticeTests
+//
+//  Created by potentia on 2026/6/12.
+//
+
+import Testing
+@testable import InterviewPractice
+
+struct InterviewPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
